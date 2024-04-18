930604a2f096bcfb3ff2844d3e8574eb7aabc106
Artic67 <sirartic67@gmail.com>
2024-04-18 23:20:05 +0300

    lab4 edge realization

--- edge/app/usecases/data_processing.py
+++ edge/app/usecases/data_processing.py
@@ -1,6 +1,20 @@
 from app.entities.agent_data import AgentData
 from app.entities.processed_agent_data import ProcessedAgentData
 
+pit_intervals = {
+    "normal": {
+        "start": 14000, 
+        "end": 18000
+        },
+    "small pits less": {
+        "start": 12000, 
+        "end": 14000
+        },
+    "small pits greater": {
+        "start": 18000, 
+        "end": 20000
+        }
+}
 
 def process_agent_data(
     agent_data: AgentData,
@@ -13,3 +27,12 @@ def process_agent_data(
         processed_data_batch (ProcessedAgentData): Processed data containing the classified state of the road surface and agent data.
     """
     # Implement it
+    z_acceleration = agent_data.accelerometer.z
+
+    if pit_intervals["normal"]["start"] < z_acceleration <= pit_intervals["normal"]["end"]:
+        road_state = "normal"
+    elif pit_intervals["small pits less"]["start"] < z_acceleration < pit_intervals["small pits less"]["end"] or pit_intervals["small pits greater"]["start"] < z_acceleration < pit_intervals["small pits greater"]["end"]:
+        road_state = "small pits"
+    else:
+        road_state = "large pits"
+    return ProcessedAgentData(road_state=road_state, agent_data=agent_data)
